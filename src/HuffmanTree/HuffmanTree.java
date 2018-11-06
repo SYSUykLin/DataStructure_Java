ba64323b4bd10cc2fe53684eb67536239816bf7c
GreenArrow2017 <1522773921@qq.com>
2018-11-06 18:20:59 +0800

    huffmanTree

--- src/HuffmanTree/HuffmanTree.java
+++ src/HuffmanTree/HuffmanTree.java
@@ -1,13 +1,76 @@
 package HuffmanTree;
 
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
 public class HuffmanTree {
+
+    public String decompress(String fileName){
+        DataInputStream in = null;
+        String srcContent = "";
+        try {
+            in = new DataInputStream(new FileInputStream(new File(fileName)));
+            Map<String, String> map = readCode(in);
+            byte [] data = readDatas(in);
+            int [] dataInt = bytes2intArray(data);
+            srcContent = Huffman2Char(map, dataInt);
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return srcContent;
+    }
+
+    private String Huffman2Char(Map<String, String> map, int [] dataInt){
+        return null;
+    }
+
+    private int [] bytes2intArray(byte [] datas){
+        int[] as = new int[datas.length];
+        for (int i = 0; i < datas.length; i++) {
+            if (datas[i] > 0){
+                as[i] = datas[i];
+            }else {
+                as[i] = datas[i] + 256;
+            }
+        }
+        return as;
+    }
+
+    private byte [] readDatas(DataInputStream in) throws IOException {
+        int dataByteNum = in.readInt();
+        byte[] bs = new byte[dataByteNum];
+        for (int i = 0; i < dataByteNum; i++) {
+            bs[i] = in.readByte();
+        }
+        return bs;
+    }
+
+    private Map<String, String> readCode(DataInputStream in){
+        Map<String, String> map = new HashMap<>();
+        try {
+            int codeNum = in.readInt();
+            for (int i = 0; i < codeNum; i++) {
+                char codeChar = in.readChar();
+                int codeLen = in.readInt();
+                String code = "";
+                char[] cs = new char[codeLen];
+                for (int j = 0; j < cs.length; j++) {
+                    code += in.readChar();
+                }
+                map.put(code, "" + codeChar);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return map;
+    }
 
     public void compress(String str, String fileName) {
         HuffmanPriorityQueue huffmanPriorityQueue = this.statistics(str);
@@ -26,67 +89,83 @@ public class HuffmanTree {
             outCode(dataOutputStream, map);
             String dataHuffmanCode = source2Code(str, map);
             outDataHuffmanCode(dataOutputStream, dataHuffmanCode);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             try {
                 dataOutputStream.close();
-            }catch (IOException e){
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
 
     private void outDataHuffmanCode(DataOutputStream os, String datacode) throws IOException {
-        byte [] bs = string2ByteArray(datacode);
+        byte[] bs = string2ByteArray(datacode);
         os.writeInt(bs.length);
         os.write(bs);
     }
 
-    private byte [] string2ByteArray(String dataHuffmanCode){
-        byte [] bytes = null;
-        char [] cs = dataHuffmanCode.toCharArray();
+    private byte[] string2ByteArray(String dataHuffmanCode) {
+        byte[] bytes = null;
+        char[] cs = dataHuffmanCode.toCharArray();
         int len = cs.length;
         int lenbytes = 0;
-        if (len % 8 == 0){
-            lenbytes = len/8 + 1;
+        if (len % 8 == 0) {
+            lenbytes = len / 8 + 1;
             bytes = new byte[lenbytes];
             for (int i = 0; i < lenbytes - 1; i++) {
                 String s = "";
-                for (int j = i*8; j < (i+1)*8; j++) {
+                for (int j = i * 8; j < (i + 1) * 8; j++) {
                     s += cs[j];
                 }
                 bytes[i] = char2bytes(s);
             }
-            bytes[lenbytes-1] = 0;
+            bytes[lenbytes - 1] = 0;
+        } else {
+            lenbytes = len / 8 + 2;
+            bytes = new byte[lenbytes];
+            int zeroNum = 8 - len % 8;
+            for (int i = 0; i < zeroNum; i++) {
+                dataHuffmanCode += "0";
+            }
+            cs = dataHuffmanCode.toCharArray();
+            for (int i = 0; i < lenbytes - 1; i++) {
+                String s = "";
+                for (int j = i * 8; j < (i + 1) * 8; j++) {
+                    s += cs[j];
+                }
+                bytes[i] = char2bytes(s);
+            }
+            bytes[lenbytes - 1] = (byte) zeroNum;
         }
-        return null;
+        return bytes;
     }
 
-    private byte char2bytes(String s){
+    private byte char2bytes(String s) {
         byte ret = 0;
         char[] cs = s.toCharArray();
 
-        for(int i=0;i<cs.length;i++){
-            byte tempB = (byte)(Byte.parseByte(""+cs[i])*Math.pow(2,cs.length-i-1));
-            ret = (byte)(ret+tempB);
+        for (int i = 0; i < cs.length; i++) {
+            byte tempB = (byte) (Byte.parseByte("" + cs[i]) * Math.pow(2, cs.length - i - 1));
+            ret = (byte) (ret + tempB);
         }
         return ret;
 
     }
 
-    private String source2Code(String string, Map<String, String> map){
+    private String source2Code(String string, Map<String, String> map) {
         StringBuffer stringBuffer = new StringBuffer();
-        char [] cs = string.toCharArray();
-        for (char c:cs) {
-            stringBuffer.append(map.get(""+c));
+        char[] cs = string.toCharArray();
+        for (char c : cs) {
+            stringBuffer.append(map.get("" + c));
         }
         return stringBuffer.toString();
     }
 
     private void outCode(DataOutputStream os, Map<String, String> code) throws IOException {
         os.writeInt(code.size());
-        for (String key:code.keySet()) {
+        for (String key : code.keySet()) {
             os.writeChar(key.charAt(0));
             os.writeInt(code.get(key).length());
             os.writeChars(code.get(key));
@@ -94,12 +173,14 @@ public class HuffmanTree {
     }
 
     private void buildHuffmanCode(Map<String, String> map, HuffmanNode huffmanTree, String stringCode) {
-        if (huffmanTree.getLeftNode() == null && huffmanTree.getRightNode() == null){
-            map.put(""+huffmanTree.getC(), stringCode);
+        if (huffmanTree.getLeftNode() == null && huffmanTree.getRightNode() == null) {
+            map.put("" + huffmanTree.getC(), stringCode);
             return;
-        }if(huffmanTree.getLeftNode() != null){
+        }
+        if (huffmanTree.getLeftNode() != null) {
             buildHuffmanCode(map, huffmanTree.getLeftNode(), stringCode + "0");
-        }if (huffmanTree.getRightNode() != null){
+        }
+        if (huffmanTree.getRightNode() != null) {
             buildHuffmanCode(map, huffmanTree.getRightNode(), stringCode + "1");
         }
     }
@@ -135,8 +216,31 @@ public class HuffmanTree {
         return huffmanPriorityQueue;
     }
 
-     public static void main(String args[]){
+    private String readFile(String fileName){
+        StringBuffer buffer = new StringBuffer();
+        DataInputStream dataInputStream = null;
+        try {
+            dataInputStream = new DataInputStream(new FileInputStream(new File(fileName)));
+            String tempSet = "";
+            while ((tempSet = dataInputStream.readLine()) != null){
+                buffer.append(tempSet + "\n");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            try {
+                dataInputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return buffer.toString();
+    }
+
+
+
+    public static void main(String args[]) {
         HuffmanTree huffmanTree = new HuffmanTree();
-        //huffmanTree.compress("aaabbcdddd");
-     }
+        huffmanTree.compress(huffmanTree.readFile("D:\\Java\\DataStructure_Java\\src\\Graph\\Version1\\DenseGraph.java"), "compress.txt");
+    }
 }
