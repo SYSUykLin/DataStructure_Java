d81bb2f5fc309b86392c1b47d8cb3801e277dcbb
GreenArrow <1522773921@qq.com>
2018-12-03 16:59:24 +0800

    BST

---
+++ src/Tree/BST/BST.java
@@ -0,0 +1,70 @@
+package Tree.BST;
+
+public class BST<E extends Comparable<E>> {
+    /**
+     * Binary Search Tree
+     */
+
+    private class Node {
+        public E e;
+        public Node left, right;
+
+        public Node(E e) {
+            this.e = e;
+            left = right = null;
+        }
+    }
+
+    private Node root;
+    private int size;
+
+    public BST() {
+        root = null;
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void add(E e) {
+        root = add(root, e);
+    }
+
+    private Node add(Node node, E e) {
+        if (node == null) {
+            size++;
+            return new Node(e);
+        }
+        if (e.compareTo(node.e) < 0) {
+            node.left = add(node.left, e);
+        } else {
+            node.right = add(node.right, e);
+        }
+        return node;
+    }
+
+    public boolean contains(E e) {
+        return contains(root, e);
+    }
+
+    public boolean contains(Node node, E e) {
+        if (node == null) {
+            return false;
+        } else if (e.equals(node.e)) {
+            return true;
+        } else if (e.compareTo(node.e) < 0) {
+            return contains(node.left, e);
+        } else {
+            return contains(node.right, e);
+        }
+    }
+
+    public Node select(E e){
+        return null;
+    }
+}
