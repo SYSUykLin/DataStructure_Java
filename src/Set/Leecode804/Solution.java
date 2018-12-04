8d7dfa549c2915d64ced7d7fb973422bac8900f0
GreenArrow <1522773921@qq.com>
2018-12-04 23:52:00 +0800

    Set

---
+++ src/Set/Leecode804/Solution.java
@@ -0,0 +1,18 @@
+package Set.Leecode804;
+
+import java.util.TreeSet;
+
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        String [] codes = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        TreeSet<String> set = new TreeSet<>();
+        for (String word : words){
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < word.length(); i++) {
+                stringBuilder.append(codes[word.charAt(i) - 'a']);
+            }
+            set.add(stringBuilder.toString());
+        }
+        return set.size();
+    }
+}
