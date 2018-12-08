09efa98524ed5b65692bbfb103aa6ca1a4cdc5b0
GreenArrow <1522773921@qq.com>
2018-12-08 23:32:15 +0800

    Segment Tree

--- src/Tree/SegmentTree/SegmentTree.java
+++ src/Tree/SegmentTree/SegmentTree.java
@@ -37,6 +37,13 @@ public class SegmentTree<E> {
         return data[index];
     }
 
+    public E getFromTree(int index) {
+        if (index < 0 || index >= 4 * data.length) {
+            throw new IllegalArgumentException("Index is illgel!");
+        }
+        return tree[index];
+    }
+
     public int getSize() {
         return data.length;
     }
@@ -47,5 +54,42 @@ public class SegmentTree<E> {
 
     private int rightChild(int index) {
         return 2 * index + 2;
+    }
+
+    public E query(int queryL, int queryR) {
+        if (queryL < 0 || queryL >= data.length || queryR < 0 || queryR >= data.length || queryL > queryR) {
+            throw new IllegalArgumentException("permater is illgel!");
+        }
+        return query(0, 0, data.length - 1, queryL, queryR);
+    }
+
+    private E query(int treeIndex, int l, int r, int queryL, int queryR) {
+        if (l == queryL && r == queryR) {
+            return tree[treeIndex];
+        }
+        int mid = l + (r - l) / 2;
+        int left = leftChild(treeIndex);
+        int right = rightChild(treeIndex);
+        if (queryL >= mid + 1) {
+            return query(right, mid + 1, r, queryL, queryR);
+        } else if (queryR <= mid) {
+            return query(left, l, mid, queryL, queryR);
+        } else {
+            return merger.merger(query(left, l, mid, queryL, mid), query(right, mid + 1, r, mid + 1, queryR));
+        }
+    }
+
+    public void show() {
+        for (int i = 0; i < tree.length; i++) {
+            System.out.print(tree[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Integer[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        SegmentTree<Integer> segmentTree = new SegmentTree<Integer>(arr, (a, b) -> a + b);
+        segmentTree.show();
+        System.out.println(segmentTree.query(2, 5));
     }
 }
