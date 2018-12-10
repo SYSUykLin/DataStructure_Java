86d79d4b56fd5692b9af0668bf09e200fdf85ded
GreenArrow <1522773921@qq.com>
2018-12-10 13:24:08 +0800

    Trie

---
+++ src/Tree/Trie/Leecode677/MapSum.java
@@ -0,0 +1,68 @@
+package Tree.Trie.Leecode677;
+
+import java.util.TreeMap;
+
+class MapSum {
+
+    private class Node {
+        public int value;
+        public TreeMap<Character, Node> next;
+
+        public Node(int value) {
+            this.value = value;
+            next = new TreeMap<>();
+        }
+
+        public Node() {
+            this(0);
+        }
+    }
+
+    private Node root;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public MapSum() {
+        root = new Node();
+    }
+
+    public void insert(String word, int val) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null) {
+                cur.next.put(c, new Node());
+            }
+            cur = cur.next.get(c);
+        }
+        cur.value = val;
+    }
+
+    public int sum(String prefix) {
+        Node cur = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if (cur.next.get(c) == null) {
+                return 0;
+            }
+            cur = cur.next.get(c);
+        }
+        return sum(cur);
+    }
+
+    private int sum(Node cur) {
+        int res = cur.value;
+        for (char key : cur.next.keySet()) {
+            res += sum(cur.next.get(key));
+        }
+        return res;
+    }
+}
+
+/**
+ * Your MapSum object will be instantiated and called as such:
+ * MapSum obj = new MapSum();
+ * obj.insert(key,val);
+ * int param_2 = obj.sum(prefix);
+ */
