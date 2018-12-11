dcbe6f11a49d0a1a3b5bb00675e31774e61cdaf7
GreenArrow <1522773921@qq.com>
2018-12-11 18:59:40 +0800

    AVL

--- src/Tree/AVL/AVL.java
+++ src/Tree/AVL/AVL.java
@@ -1,52 +1,215 @@
 package Tree.AVL;
 
+import java.util.Random;
+
 public class AVL<K extends Comparable<K>, V> {
-    private class Node{
+    private class Node {
         public K key;
         public V value;
         public Node left, right;
         public int height;
-        public Node(K key, V value){
+
+        public Node(K key, V value) {
             this.key = key;
             this.value = value;
             left = right = null;
             height = 1;
         }
     }
+
     private Node root;
     private int size;
-    public AVL(){
+
+    public AVL() {
         root = null;
         size = 0;
     }
-    public int getSize(){
+
+    public int getSize() {
         return size;
     }
-    public boolean isEmpty(){
+
+    public boolean isEmpty() {
         return size == 0;
     }
-    private int getHeight(Node node){
-        if (node == null){
+
+    private int getHeight(Node node) {
+        if (node == null) {
             return 0;
-        }else {
+        } else {
             return node.height;
         }
     }
-    public void add(K key, V value){
-        root = add(root, key, value);
-    }
-    public Node add(Node node, K key, V value){
-        if (node == null){
-            size ++;
-            return new Node(key, value);
+
+    private V minimum() {
+        if (size == 0) {
+            return null;
         }
-        if (key.compareTo(node.key) > 0){
-            node.right = add(node.right, key, value);
-        }else if(key.compareTo(node.key) < 0){
-            node.left = add(node.left, key, value);
-        }else {
-            node.value = value;
+        return minimum(root).value;
+    }
+
+    private Node minimum(Node node) {
+        if (node == null) {
+            return null;
+        }
+        if (node.left == null) {
+            return node;
+        } else {
+            return minimum(node.left);
+        }
+    }
+
+    private V removeMin() {
+        V ret = minimum();
+        if (size == 0) {
+            return null;
+        }
+        root = removeMin(root);
+        return ret;
+    }
+
+    private Node removeMin(Node node) {
+        if (node.left == null) {
+            Node right = node.right;
+            node.right = null;
+            size--;
+        }
+        node.left = removeMin(node.left);
+        return node;
+    }
+
+    private void remove(K key) {
+        root = remove(root, key);
+    }
+
+    private Node remove(Node node, K key) {
+        if (key.compareTo(node.key) < 0) {
+            node.left = remove(node.left, key);
+        } else if (key.compareTo(node.key) > 0) {
+            node.right = remove(node.right, key);
+        } else {
+            if (node.left == null) {
+                Node right = node.right;
+                node.right = null;
+                size--;
+                return right;
+            } else if (node.right == null) {
+                Node left = node.left;
+                node.left = null;
+                size--;
+                return left;
+            } else {
+                Node successor = new Node(minimum(node.right).key, minimum(node.right).value);
+                node.right = removeMin(node.right);
+                successor.left = node.left;
+                successor.right = node.right;
+                node.right = node.left = null;
+                return successor;
+            }
+        }
+        node.height = 1 + Math.max(getHeight(node.left), getHeight(node.right));
+        int balanceFactor = getBalanceFactor(node);
+        if (balanceFactor > 1 && getBalanceFactor(node.left) >= 0) {
+            return rightRotate(node);
+        }
+        if (balanceFactor < -1 && getBalanceFactor(node.right) <= 0) {
+            return leftRotate(node);
+        }
+        if (balanceFactor > 1 && getBalanceFactor(node.left) < 0) {
+            node.left = leftRotate(node.left);
+            return rightRotate(node);
+        }
+        if (balanceFactor < -1 && getBalanceFactor(node.right) > 0) {
+            node.right = rightRotate(node.right);
+            return leftRotate(node);
         }
         return node;
+
+    }
+
+    public void add(K key, V value) {
+        root = add(root, key, value);
+    }
+
+    public Node add(Node node, K key, V value) {
+        if (node == null) {
+            size++;
+            return new Node(key, value);
+        }
+        if (key.compareTo(node.key) > 0) {
+            node.right = add(node.right, key, value);
+        } else if (key.compareTo(node.key) < 0) {
+            node.left = add(node.left, key, value);
+        } else {
+            node.value = value;
+        }
+        node.height = 1 + Math.max(getHeight(node.left), getHeight(node.right));
+        int balanceFactor = getBalanceFactor(node);
+        if (balanceFactor > 1 && getBalanceFactor(node.left) >= 0) {
+            return rightRotate(node);
+        }
+        if (balanceFactor < -1 && getBalanceFactor(node.right) <= 0) {
+            return leftRotate(node);
+        }
+        if (balanceFactor > 1 && getBalanceFactor(node.left) < 0) {
+            node.left = leftRotate(node.left);
+            return rightRotate(node);
+        }
+        if (balanceFactor < -1 && getBalanceFactor(node.right) > 0) {
+            node.right = rightRotate(node.right);
+            return leftRotate(node);
+        }
+        //System.out.println(getBalanceFactor(node));
+        return node;
+    }
+
+    private Node rightRotate(Node y) {
+        Node x = y.left;
+        Node T3 = x.right;
+        x.right = y;
+        y.left = T3;
+        y.height = 1 + Math.max(getHeight(y.left), getHeight(y.right));
+        x.height = 1 + Math.max(getHeight(x.left), getHeight(x.right));
+        return x;
+    }
+
+    private Node leftRotate(Node y) {
+        Node x = y.right;
+        Node T2 = x.left;
+        x.left = y;
+        y.right = T2;
+        y.height = 1 + Math.max(getHeight(y.left), getHeight(y.right));
+        x.height = 1 + Math.max(getHeight(x.left), getHeight(x.right));
+        return x;
+    }
+
+    private int getBalanceFactor(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        return getHeight(node.left) - getHeight(node.right);
+    }
+
+    public void show(){
+        show(root);
+        System.out.println();
+    }
+
+    public void show(Node node) {
+        if (node == null) {
+            return;
+        }
+        show(node.left);
+        System.out.print("[" + node.key + " " + node.value + "]");
+        show(node.right);
+    }
+
+    public static void main(String[] args) {
+        AVL<Integer, Integer> avl = new AVL<>();
+        Random random = new Random();
+        for (int i = 0; i < 100; i++) {
+            avl.add(random.nextInt(), random.nextInt());
+        }
+        avl.show();
     }
 }
