3ae3467e816bb5e946ac90ce562ad4e2ad2c29bf
GreenArrow <1522773921@qq.com>
2018-12-13 20:54:35 +0800

    RBTree

--- src/Tree/RBTree/RBTree.java
+++ src/Tree/RBTree/RBTree.java
@@ -22,7 +22,14 @@ public class RBTree<T extends Comparable<T>> {
 
         @Override
         public String toString() {
-            return "" + key + (this.color == RED ? "R" : "B");
+            String s = "" + key + (this.color == RED ? "R" : "B");
+            if (this.left != null) {
+                s += ("  " + left.key);
+            }
+            if (this.right != null) {
+                s += ("  " + right.key);
+            }
+            return s;
         }
     }
 
@@ -56,50 +63,402 @@ public class RBTree<T extends Comparable<T>> {
         }
     }
 
-    private void setRed(Node<T> node){
-        if (node != null){
+    private void setRed(Node<T> node) {
+        if (node != null) {
             node.color = RED;
         }
     }
 
-    private void setParent(Node<T> node, Node<T> parent){
-        if (node != null){
+    private void setParent(Node<T> node, Node<T> parent) {
+        if (node != null) {
             node.parent = parent;
         }
     }
 
-    private void setColor(Node<T> node, boolean color){
-        if (node != null){
+    private void setColor(Node<T> node, boolean color) {
+        if (node != null) {
             node.color = color;
         }
     }
 
-    public void inOrder(){
+    public void inOrder() {
         inOrder(root);
     }
 
-    private void inOrder(Node<T> tree){
-        if (tree != null){
+    private void inOrder(Node<T> tree) {
+        if (tree != null) {
             inOrder(tree.left);
             System.out.println(tree);
             inOrder(tree.right);
         }
     }
 
-    public Node<T> search(T key){
+    public Node<T> search(T key) {
         return search(root, key);
     }
 
-    private Node<T> search(Node<T> node, T key){
-        if (node == null){
+    private Node<T> search(Node<T> node, T key) {
+        if (node == null) {
             return null;
-        }
-        else if (key.compareTo(node.key) < 0){
+        } else if (key.compareTo(node.key) < 0) {
             return search(node.left, key);
-        }else if (key.compareTo(node.key) > 0){
+        } else if (key.compareTo(node.key) > 0) {
             return search(node.right, key);
-        }else {
+        } else {
             return node;
         }
+    }
+
+    public T minimum() {
+        Node<T> p = minimum(root);
+        if (p != null) {
+            return p.key;
+        }
+        return null;
+    }
+
+    private Node<T> minimum(Node<T> node) {
+        if (node == null) {
+            return null;
+        }
+        while (node.left != null) {
+            node = node.left;
+        }
+        return node;
+    }
+
+    public T maximum() {
+        Node<T> p = maximum(root);
+        if (p != null) {
+            return p.key;
+        }
+        return null;
+    }
+
+    private Node<T> maximum(Node<T> node) {
+        if (node == null) {
+            return null;
+        }
+        while (node.right != null) {
+            node = node.right;
+        }
+        return node;
+    }
+
+    /*
+    find the successor of the current node,
+    successor is the smallest over the node which bigger than current node
+     */
+    public Node<T> successor(Node<T> x) {
+        if (x.right != null) {
+            return minimum(x.right);
+        }
+
+        Node<T> y = x.parent;
+        while ((y != null) && (x == y.right)) {
+            x = y;
+            y = y.parent;
+        }
+        return y;
+    }
+
+    /*
+    find the predecessor of the current node,
+    predecessor is the biggest over the node which smaller than the current node.
+     */
+    public Node<T> predecessor(Node<T> x) {
+        if (x.left != null) {
+            return maximum(x.left);
+        }
+        Node<T> y = x.parent;
+        while (y != null && x == y.left) {
+            x = y;
+            y = y.parent;
+        }
+        return y;
+    }
+
+    private void leftRotate(Node<T> x) {
+        Node<T> y = x.right;
+        x.right = y.left;
+        if (y.left != null) {
+            y.left.parent = x;
+        }
+        y.parent = x.parent;
+        if (x.parent == null) {
+            this.root = y;
+        } else {
+            if (x.parent.left == x) {
+                x.parent.left = y;
+            } else {
+                x.parent.right = y;
+            }
+        }
+
+        y.left = x;
+        x.parent = y;
+    }
+
+    private void rightRotate(Node<T> y) {
+        Node<T> x = y.left;
+        y.left = x.right;
+        if (x.right != null) {
+            x.right.parent = y;
+        }
+        x.parent = y.parent;
+        if (y.parent == null) {
+            this.root = x;
+        } else {
+            if (y.parent.left == y) {
+                y.parent.left = x;
+            } else {
+                y.parent.right = x;
+            }
+        }
+        x.right = y;
+        y.parent = x;
+    }
+
+    private void insertFixUp(Node<T> node) {
+        Node<T> parent, grandParent;
+        while ((parent = parentOf(node)) != null && isRed(parent)) {
+            grandParent = parentOf(parent);
+            if (parent == grandParent.left) {
+                //third condition,the uncle is red
+                Node<T> uncle = grandParent.right;
+                if (uncle != null && isRed(uncle)) {
+                    setBlack(uncle);
+                    setBlack(parent);
+                    setRed(grandParent);
+                    node = grandParent;
+                    continue;
+                }
+                //fifth condition,the uncle is black and left child of the current node
+                if (parent.right == node) {
+                    Node<T> temp;
+                    leftRotate(parent);
+                    temp = parent;
+                    parent = node;
+                    node = temp;
+                }
+                //fourth condiction,same as above but the right child of the current node.
+                setBlack(parent);
+                setRed(grandParent);
+                rightRotate(grandParent);
+            } else {
+                //third condition,the uncle is red.
+                Node<T> uncle = grandParent.left;
+                if (uncle != null && isRed(uncle)) {
+                    setBlack(uncle);
+                    setBlack(parent);
+                    setRed(grandParent);
+                    node = grandParent;
+                    continue;
+                }
+                //fifth condition,the uncle is black,left child
+                if (parent.left == node) {
+                    Node<T> temp;
+                    rightRotate(parent);
+                    temp = parent;
+                    parent = node;
+                    node = temp;
+                }
+                //fourth condition,the uncle is black,right child
+                setBlack(parent);
+                setRed(grandParent);
+                leftRotate(grandParent);
+            }
+        }
+        setBlack(root);
+    }
+
+    private void remove(Node<T> node) {
+        Node<T> child, parent;
+        boolean color;
+        if (node.left != null && node.right != null) {
+            Node<T> replacer = node;
+            replacer = minimum(replacer.right);
+            if (parentOf(node) != null) {
+                if (parentOf(node).left == node) {
+                    parentOf(node).left = replacer;
+                } else {
+                    parentOf(node).right = replacer;
+                }
+            } else {
+                this.root = replacer;
+            }
+            child = replacer.right;
+            parent = parentOf(replacer);
+            color = colorOf(replacer);
+            if (parent == node) {
+                parent = replacer;
+            } else {
+                if (child != null) {
+                    setParent(child, parent);
+                }
+                parent.left = child;
+                replacer.right = node.right;
+                setParent(node.right, replacer);
+            }
+            replacer.parent = node.parent;
+            replacer.color = node.color;
+            replacer.left = node.left;
+            node.left.parent = replacer;
+            if (color == BLACK) {
+                removeFixUp(replacer, parent);
+            }
+            node = null;
+            return;
+        }
+
+        if (node.left != null) {
+            child = node.left;
+        } else {
+            child = node.right;
+        }
+        parent = node.parent;
+        color = node.color;
+        if (child != null) {
+            child.parent = parent;
+        }
+        if (parent != null) {
+            if (parent.left == node) {
+                parent.left = child;
+            } else {
+                parent.right = child;
+            }
+        } else {
+            this.root = child;
+        }
+
+        setColor(child, color);
+        if (color == BLACK) {
+            removeFixUp(child, parent);
+        }
+        node = null;
+    }
+
+    public void remove(T key) {
+        Node<T> node;
+
+        if ((node = search(root, key)) != null)
+            remove(node);
+    }
+
+    public void insert(T e) {
+        Node<T> node = new Node<T>(e, RED, null, null, null);
+
+        // 如果新建结点失败，则返回。
+        if (node != null)
+            insert(node);
+    }
+
+    private void insert(Node<T> node) {
+        int cmp;
+        Node<T> y = null;
+        Node<T> x = root;
+        while (x != null) {
+            y = x;
+            cmp = node.key.compareTo(x.key);
+            if (cmp < 0) {
+                x = x.left;
+            } else {
+                x = x.right;
+            }
+        }
+        node.parent = y;
+        if (y != null) {
+            cmp = node.key.compareTo(y.key);
+            if (cmp < 0) {
+                y.left = node;
+            } else {
+                y.right = node;
+            }
+        } else {
+            this.root = node;
+        }
+
+        node.color = RED;
+        insertFixUp(node);
+    }
+
+    private void removeFixUp(Node<T> node, Node<T> par) {
+        Node<T> uncle;
+        Node<T> parent;
+        parent = node == null ? par : node.parent;
+        while ((node == null || isBlack(node)) && node != root) {
+            if (parent.left == node) {
+                uncle = parent.right;
+                //the uncle is red, condition three
+                if (isRed(uncle)) {
+                    setBlack(uncle);
+                    setRed(parent);
+                    leftRotate(parent);
+                    uncle = parent.right;
+                }
+                //the uncle and his child are all black
+                if ((uncle.left == null || isBlack(uncle.left)) &&
+                        (uncle.right == null || isBlack(uncle.right))) {
+                    setRed(uncle);
+                    node = parent;
+                    parent = parentOf(node);
+                } else {
+                    //the uncle is black and red of his child on the left
+                    if (uncle.right == null || isBlack(uncle.right)) {
+                        setBlack(uncle.left);
+                        setRed(uncle);
+                        rightRotate(uncle);
+                        uncle = parent.right;
+                    }
+                    setColor(uncle, colorOf(parent));
+                    setBlack(parent);
+                    setBlack(uncle.right);
+                    leftRotate(parent);
+                    node = this.root;
+                    break;
+                }
+            } else {
+                uncle = parent.left;
+                if (isRed(uncle)) {
+                    setBlack(uncle);
+                    setRed(parent);
+                    rightRotate(parent);
+                    uncle = parent.left;
+                }
+                if ((uncle.left == null || isBlack(uncle.left)) &&
+                        (uncle.right == null || isBlack(uncle.right))) {
+                    setRed(uncle);
+                    node = parent;
+                    parent = parentOf(node);
+                } else {
+                    if (uncle.left == null || isBlack(uncle.left)) {
+                        setBlack(uncle.right);
+                        setRed(uncle);
+                        leftRotate(uncle);
+                        uncle = parent.left;
+                    }
+                    setColor(uncle, colorOf(parent));
+                    setBlack(parent);
+                    setBlack(uncle.left);
+                    rightRotate(parent);
+                    node = this.root;
+                    break;
+                }
+            }
+        }
+        if (node != null) {
+            setBlack(node);
+        }
+    }
+
+    public static void main(String[] args) {
+        RBTree<Integer> rbTree = new RBTree<>();
+        int a[] = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+        for (int i = 0; i < a.length; i++) {
+            rbTree.insert(a[i]);
+        }
+        rbTree.inOrder();
+        rbTree.remove(30);
+        rbTree.inOrder();
     }
 }
--- src/Tree/RBTree/Test.java
+++ src/Tree/RBTree/Test.java
@@ -11,7 +11,7 @@ public class Test<T extends Comparable<T>> {
 
     private RBTNode<T> mRoot;    // 根结点
 
-    private static final boolean RED   = false;
+    private static final boolean RED = false;
     private static final boolean BLACK = true;
 
     public class RBTNode<T extends Comparable<T>> {
@@ -34,40 +34,47 @@ public class Test<T extends Comparable<T>> {
         }
 
         public String toString() {
-            return ""+key+(this.color==RED?"(R)":"B");
+            return "" + key + (this.color == RED ? "(R)" : "B");
         }
     }
 
     public Test() {
-        mRoot=null;
+        mRoot = null;
     }
 
     private RBTNode<T> parentOf(RBTNode<T> node) {
-        return node!=null ? node.parent : null;
+        return node != null ? node.parent : null;
     }
+
     private boolean colorOf(RBTNode<T> node) {
-        return node!=null ? node.color : BLACK;
+        return node != null ? node.color : BLACK;
     }
+
     private boolean isRed(RBTNode<T> node) {
-        return ((node!=null)&&(node.color==RED)) ? true : false;
+        return ((node != null) && (node.color == RED)) ? true : false;
     }
+
     private boolean isBlack(RBTNode<T> node) {
         return !isRed(node);
     }
+
     private void setBlack(RBTNode<T> node) {
-        if (node!=null)
+        if (node != null)
             node.color = BLACK;
     }
+
     private void setRed(RBTNode<T> node) {
-        if (node!=null)
+        if (node != null)
             node.color = RED;
     }
+
     private void setParent(RBTNode<T> node, RBTNode<T> parent) {
-        if (node!=null)
+        if (node != null)
             node.parent = parent;
     }
+
     private void setColor(RBTNode<T> node, boolean color) {
-        if (node!=null)
+        if (node != null)
             node.color = color;
     }
 
@@ -75,8 +82,8 @@ public class Test<T extends Comparable<T>> {
      * 前序遍历"红黑树"
      */
     private void preOrder(RBTNode<T> tree) {
-        if(tree != null) {
-            System.out.print(tree.key+" ");
+        if (tree != null) {
+            System.out.print(tree.key + " ");
             preOrder(tree.left);
             preOrder(tree.right);
         }
@@ -90,9 +97,9 @@ public class Test<T extends Comparable<T>> {
      * 中序遍历"红黑树"
      */
     private void inOrder(RBTNode<T> tree) {
-        if(tree != null) {
+        if (tree != null) {
             inOrder(tree.left);
-            System.out.print(tree.key+" ");
+            System.out.print(tree.key + " ");
             inOrder(tree.right);
         }
     }
@@ -106,11 +113,10 @@ public class Test<T extends Comparable<T>> {
      * 后序遍历"红黑树"
      */
     private void postOrder(RBTNode<T> tree) {
-        if(tree != null)
-        {
+        if (tree != null) {
             postOrder(tree.left);
             postOrder(tree.right);
-            System.out.print(tree.key+" ");
+            System.out.print(tree.key + " ");
         }
     }
 
@@ -123,7 +129,7 @@ public class Test<T extends Comparable<T>> {
      * (递归实现)查找"红黑树x"中键值为key的节点
      */
     private RBTNode<T> search(RBTNode<T> x, T key) {
-        if (x==null)
+        if (x == null)
             return x;
 
         int cmp = key.compareTo(x.key);
@@ -143,7 +149,7 @@ public class Test<T extends Comparable<T>> {
      * (非递归实现)查找"红黑树x"中键值为key的节点
      */
     private RBTNode<T> iterativeSearch(RBTNode<T> x, T key) {
-        while (x!=null) {
+        while (x != null) {
             int cmp = key.compareTo(x.key);
 
             if (cmp < 0)
@@ -168,7 +174,7 @@ public class Test<T extends Comparable<T>> {
         if (tree == null)
             return null;
 
-        while(tree.left != null)
+        while (tree.left != null)
             tree = tree.left;
         return tree;
     }
@@ -188,7 +194,7 @@ public class Test<T extends Comparable<T>> {
         if (tree == null)
             return null;
 
-        while(tree.right != null)
+        while (tree.right != null)
             tree = tree.right;
         return tree;
     }
@@ -213,7 +219,7 @@ public class Test<T extends Comparable<T>> {
         // (01) x是"一个左孩子"，则"x的后继结点"为 "它的父结点"。
         // (02) x是"一个右孩子"，则查找"x的最低的父结点，并且该父结点要具有左孩子"，找到的这个"最低的父结点"就是"x的后继结点"。
         RBTNode<T> y = x.parent;
-        while ((y!=null) && (x==y.right)) {
+        while ((y != null) && (x == y.right)) {
             x = y;
             y = y.parent;
         }
@@ -233,7 +239,7 @@ public class Test<T extends Comparable<T>> {
         // (01) x是"一个右孩子"，则"x的前驱结点"为 "它的父结点"。
         // (01) x是"一个左孩子"，则查找"x的最低的父结点，并且该父结点要具有右孩子"，找到的这个"最低的父结点"就是"x的前驱结点"。
         RBTNode<T> y = x.parent;
-        while ((y!=null) && (x==y.left)) {
+        while ((y != null) && (x == y.left)) {
             x = y;
             y = y.parent;
         }
@@ -248,10 +254,10 @@ public class Test<T extends Comparable<T>> {
      *      px                              px
      *     /                               /
      *    x                               y
-     *   /  \      --(左旋)-.           / \                #
+     *   /  \      --(左旋)-.             / \                #
      *  lx   y                          x  ry
-     *     /   \                       /  \
-     *    ly   ry                     lx  ly
+     *     /   \                       / \
+     *    ly   ry                    lx  ly
      *
      *
      */
@@ -290,7 +296,7 @@ public class Test<T extends Comparable<T>> {
      *            py                               py
      *           /                                /
      *          y                                x
-     *         /  \      --(右旋)-.            /  \                     #
+     *         /  \      --(右旋)-.             /   \                     #
      *        x   ry                           lx   y
      *       / \                                   / \                   #
      *      lx  rx                                rx  ry
@@ -338,14 +344,14 @@ public class Test<T extends Comparable<T>> {
         RBTNode<T> parent, gparent;
 
         // 若“父节点存在，并且父节点的颜色是红色”
-        while (((parent = parentOf(node))!=null) && isRed(parent)) {
+        while (((parent = parentOf(node)) != null) && isRed(parent)) {
             gparent = parentOf(parent);
 
             //若“父节点”是“祖父节点的左孩子”
             if (parent == gparent.left) {
                 // Case 1条件：叔叔节点是红色
                 RBTNode<T> uncle = gparent.right;
-                if ((uncle!=null) && isRed(uncle)) {
+                if ((uncle != null) && isRed(uncle)) {
                     setBlack(uncle);
                     setBlack(parent);
                     setRed(gparent);
@@ -369,7 +375,7 @@ public class Test<T extends Comparable<T>> {
             } else {    //若“z的父节点”是“z的祖父节点的右孩子”
                 // Case 1条件：叔叔节点是红色
                 RBTNode<T> uncle = gparent.left;
-                if ((uncle!=null) && isRed(uncle)) {
+                if ((uncle != null) && isRed(uncle)) {
                     setBlack(uncle);
                     setBlack(parent);
                     setRed(gparent);
@@ -419,7 +425,7 @@ public class Test<T extends Comparable<T>> {
         }
 
         node.parent = y;
-        if (y!=null) {
+        if (y != null) {
             cmp = node.key.compareTo(y.key);
             if (cmp < 0)
                 y.left = node;
@@ -443,7 +449,7 @@ public class Test<T extends Comparable<T>> {
      *     key 插入结点的键值
      */
     public void insert(T key) {
-        RBTNode<T> node=new RBTNode<T>(key,BLACK,null,null,null);
+        RBTNode<T> node = new RBTNode<T>(key, BLACK, null, null, null);
 
         // 如果新建结点失败，则返回。
         if (node != null)
@@ -463,7 +469,7 @@ public class Test<T extends Comparable<T>> {
     private void removeFixUp(RBTNode<T> node, RBTNode<T> parent) {
         RBTNode<T> other;
 
-        while ((node==null || isBlack(node)) && (node != this.mRoot)) {
+        while ((node == null || isBlack(node)) && (node != this.mRoot)) {
             if (parent.left == node) {
                 other = parent.right;
                 if (isRed(other)) {
@@ -474,15 +480,15 @@ public class Test<T extends Comparable<T>> {
                     other = parent.right;
                 }
 
-                if ((other.left==null || isBlack(other.left)) &&
-                        (other.right==null || isBlack(other.right))) {
+                if ((other.left == null || isBlack(other.left)) &&
+                        (other.right == null || isBlack(other.right))) {
                     // Case 2: x的兄弟w是黑色，且w的俩个孩子也都是黑色的
                     setRed(other);
                     node = parent;
                     parent = parentOf(node);
                 } else {
 
-                    if (other.right==null || isBlack(other.right)) {
+                    if (other.right == null || isBlack(other.right)) {
                         // Case 3: x的兄弟w是黑色的，并且w的左孩子是红色，右孩子为黑色。
                         setBlack(other.left);
                         setRed(other);
@@ -508,15 +514,15 @@ public class Test<T extends Comparable<T>> {
                     other = parent.left;
                 }
 
-                if ((other.left==null || isBlack(other.left)) &&
-                        (other.right==null || isBlack(other.right))) {
+                if ((other.left == null || isBlack(other.left)) &&
+                        (other.right == null || isBlack(other.right))) {
                     // Case 2: x的兄弟w是黑色，且w的俩个孩子也都是黑色的
                     setRed(other);
                     node = parent;
                     parent = parentOf(node);
                 } else {
 
-                    if (other.left==null || isBlack(other.left)) {
+                    if (other.left == null || isBlack(other.left)) {
                         // Case 3: x的兄弟w是黑色的，并且w的左孩子是红色，右孩子为黑色。
                         setBlack(other.right);
                         setRed(other);
@@ -535,7 +541,7 @@ public class Test<T extends Comparable<T>> {
             }
         }
 
-        if (node!=null)
+        if (node != null)
             setBlack(node);
     }
 
@@ -550,7 +556,7 @@ public class Test<T extends Comparable<T>> {
         boolean color;
 
         // 被删除节点的"左右孩子都不为空"的情况。
-        if ( (node.left!=null) && (node.right!=null) ) {
+        if ((node.left != null) && (node.right != null)) {
             // 被删节点的后继节点。(称为"取代节点")
             // 用它来取代"被删节点"的位置，然后再将"被删节点"去掉。
             RBTNode<T> replace = node;
@@ -561,7 +567,7 @@ public class Test<T extends Comparable<T>> {
                 replace = replace.left;
 
             // "node节点"不是根节点(只有根节点不存在父节点)
-            if (parentOf(node)!=null) {
+            if (parentOf(node) != null) {
                 if (parentOf(node).left == node)
                     parentOf(node).left = replace;
                 else
@@ -583,7 +589,7 @@ public class Test<T extends Comparable<T>> {
                 parent = replace;
             } else {
                 // child不为空
-                if (child!=null)
+                if (child != null)
                     setParent(child, parent);
                 parent.left = child;
 
@@ -600,10 +606,10 @@ public class Test<T extends Comparable<T>> {
                 removeFixUp(child, parent);
 
             node = null;
-            return ;
+            return;
         }
 
-        if (node.left !=null) {
+        if (node.left != null) {
             child = node.left;
         } else {
             child = node.right;
@@ -613,11 +619,11 @@ public class Test<T extends Comparable<T>> {
         // 保存"取代节点"的颜色
         color = node.color;
 
-        if (child!=null)
+        if (child != null)
             child.parent = parent;
 
         // "node节点"不是根节点
-        if (parent!=null) {
+        if (parent != null) {
             if (parent.left == node)
                 parent.left = child;
             else
@@ -649,15 +655,15 @@ public class Test<T extends Comparable<T>> {
      * 销毁红黑树
      */
     private void destroy(RBTNode<T> tree) {
-        if (tree==null)
-            return ;
+        if (tree == null)
+            return;
 
         if (tree.left != null)
             destroy(tree.left);
         if (tree.right != null)
             destroy(tree.right);
 
-        tree=null;
+        tree = null;
     }
 
     public void clear() {
@@ -675,15 +681,15 @@ public class Test<T extends Comparable<T>> {
      */
     private void print(RBTNode<T> tree, T key, int direction) {
 
-        if(tree != null) {
+        if (tree != null) {
 
-            if(direction==0)    // tree是根节点
+            if (direction == 0)    // tree是根节点
                 System.out.printf("%2d(B) is root\n", tree.key);
             else                // tree是分支节点
-                System.out.printf("%2d(%s) is %2d's %6s child\n", tree.key, isRed(tree)?"R":"B", key, direction==1?"right" : "left");
+                System.out.printf("%2d(%s) is %2d's %6s child\n", tree.key, isRed(tree) ? "R" : "B", key, direction == 1 ? "right" : "left");
 
             print(tree.left, tree.key, -1);
-            print(tree.right,tree.key,  1);
+            print(tree.right, tree.key, 1);
         }
     }
 
